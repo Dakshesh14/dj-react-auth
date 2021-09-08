aa8d51e616400c1489819f3c36071480a20f545a
Dev-Jain04 <dakshesh.jain14@gmail.com>
2021-09-08 23:02:36 +0530

    login page done with formik and yup

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,53 @@
-import React, { memo } from 'react'
+import React, { memo } from 'react';
+
+import {
+    Link,
+} from 'react-router-dom';
+
+
+import { Formik, Form } from 'formik';
+import TextField from './TextField';
+import * as Yup from 'yup';
 
 export default memo(function Login() {
+
+    const validate = Yup.object({
+        username: Yup.string()
+            .required('Required'),
+        password: Yup.string()
+            .required('Password is required')
+    })
+
+
     return (
-        <h1 className="mt-5">
-            Login Page
-        </h1>
+        <div className="container mt-5 pt-5">
+            <div className="row">
+                <div className="col-lg-6 mx-auto">
+                    <h2 className="fs-1 mb-5">
+                        Login
+                    </h2>
+                    <Formik
+                        initialValues={{
+                            username: '',
+                            password: '',
+                        }}
+                        validationSchema={validate}
+                        onSubmit={values => {
+                            console.log(values)
+                        }}
+                    >
+                        <Form>
+                            <TextField label="Username" name="username" type="text" />
+                            <TextField label="Password" name="password" type="password" />
+                            <p className="text-muted">
+                                Don't have an account?
+                                <Link to="/register" className="ms-1">Register</Link>
+                            </p>
+                            <button className="btn btn-dark mt-3 float-end" type="submit">Login</button>
+                        </Form>
+                    </Formik>
+                </div>
+            </div>
+        </div>
     )
 })
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -19,7 +19,7 @@ export default memo(function Register() {
             .email('Email is invalid')
             .required('Email is required'),
         password: Yup.string()
-            .min(6, 'Password must be at least 8 charaters')
+            .min(8, 'Password must be at least 8 charaters')
             .required('Password is required')
             .matches(
                 /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
